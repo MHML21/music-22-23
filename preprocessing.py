69c3ca829a1f558541871bff44c13c10411c34ea
maxgolub <maxgolub@umich.edu>
2022-11-10 20:30:46 -0500

    tweaked preprocessing to output 1D array: values 0-128 0 means no note played

--- preprocessing.py
+++ preprocessing.py
@@ -3,11 +3,14 @@ from dataset import *
 import numpy as np
 import pandas as pd
 import pypianoroll
+import sys
 
 # pandas printing settings
 pd.set_option('max_colwidth', None)
 pd.options.display.max_columns = None
 pd.options.display.width = 0
+np.set_printoptions(threshold=sys.maxsize)
+#np.set_printoptions(linewidth=np.inf)
 
 
 def get_file_paths(genre: str) -> tuple[str]:
@@ -28,7 +31,6 @@ def clean_multitrack(multitrack: pypianoroll.Multitrack, track: str) -> np.ndarr
         'Bass': 3,
         'Strings': 4,
     }
-    multitrack.binarize()
     try:
         multitrack.trim()
     except ValueError:
@@ -41,11 +43,20 @@ def load_tracks_from_genre(genre: str, track_type: str, verbose: bool = True) ->
 
     multitracks = []
     for i, path in enumerate(paths):
-        if verbose and (i % 100 == 0):
+        if(i == 129):
+            break
+        if verbose and (i % 10 == 0):
             print(f'Loading track {i}...')
-        multitracks.append(clean_multitrack(pypianoroll.load(path), track_type))
-    return np.concatenate(multitracks, axis=0)
+        multitracks.append(one_multitrack_parse(path, genre, track_type))
+    return multitracks
 
-
+def one_multitrack_parse(path: str, genre: str, track_type: str):
+    multi = pypianoroll.load(path)
+    cleansed = clean_multitrack(multi, track_type).T
+    mask = cleansed != 0
+    axis = 0
+    not_played_val = -1
+    note_vals = np.where(mask.any(axis=axis), mask.argmax(axis=axis), not_played_val)
+    return np.trim_zeros(note_vals + 1)
 if __name__ == '__main__':
-    combined = load_tracks_from_genre('rock', 'Strings')
+    one_dim_tracks = load_tracks_from_genre('rock', 'Guitar')
